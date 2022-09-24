f417227bfe2823bc77f415357910cc3238a5fd9c
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 00:46:11 +0300

    Add files via upload

---
+++ Sum Digits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int numCopy = number;
+            int digitSum = 0;
+            while (numCopy>0)
+            {
+                digitSum += numCopy % 10;
+                numCopy /= 10;
+            }
+            Console.WriteLine(digitSum);
+        }
+    }
+}
