3316cffc69ab1704dee17279ec8a08aedde30e5c
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 01:14:00 +0300

    Add files via upload

---
+++ Elevator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countOfPeople = int.Parse(Console.ReadLine());
+            short capacity = short.Parse(Console.ReadLine());
+
+            int cast = (int)capacity;
+            if (capacity >= countOfPeople)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                if (countOfPeople % capacity == 0)
+                {
+                    Console.WriteLine(countOfPeople/capacity);
+                }
+                else
+                {
+                    Console.WriteLine(countOfPeople/capacity + 1);
+                }
+            }
+        }
+    }
+}
