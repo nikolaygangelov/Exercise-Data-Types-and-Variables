107ca3fc83f06af2b2f68bdbb2f0e5b124a797bb
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 01:49:19 +0300

    Add files via upload

---
+++ Sum of Chars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char? character = null;
+            int sumOfAscii = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                character = char.Parse(Console.ReadLine());
+                sumOfAscii += (int)character;
+            }
+            Console.WriteLine($"The sum equals: {sumOfAscii}");
+        }
+    }
+}
