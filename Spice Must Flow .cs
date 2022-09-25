e12eb6ab54ed5e2cf24418380d7665c05794723c
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 23:18:03 +0300

    Add files via upload

---
+++ Spice Must Flow .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+
+            int countOfDays = 0;
+            int expectedYield = startingYield;
+            int totalAmountOfSpice = 0;
+            while (expectedYield >= 100)
+            {
+                countOfDays++;
+                totalAmountOfSpice += expectedYield - 26;
+                expectedYield -= 10;
+            }
+            Console.WriteLine(countOfDays);
+            Console.WriteLine(totalAmountOfSpice-26);
+        }
+    }
+}
