5d4395fb5d785e1259b32dcc648dca84064bf0e4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 16:03:56 +0300

    Add files via upload

---
+++ Print Part of the ASCII Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._Print_Part_of_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte firstNumber = byte.Parse(Console.ReadLine());
+            byte lastNumber = byte.Parse(Console.ReadLine());
+
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
