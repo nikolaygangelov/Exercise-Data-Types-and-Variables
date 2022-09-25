04bf66b2c58a1d26225502a918e7fb77de8be45d
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-25 20:13:38 +0300

    Add files via upload

---
+++ Water Overflow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fdsfsdfdsfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int quantities = int.Parse(Console.ReadLine());
+                if (quantities <= 255)
+                {
+                    totalSum += quantities;
+                    if (totalSum > 255)
+                    {
+                        Console.WriteLine($"Insufficient capacity!");
+                        totalSum -= quantities;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
