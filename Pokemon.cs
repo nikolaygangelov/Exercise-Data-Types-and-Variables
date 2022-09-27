a08c06438f92e530c396ac0c4d38e73b7285da46
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-27 16:28:58 +0300

    Add files via upload

---
+++ Pokemon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _10._Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+            int currentN = N;
+            int countOfTargets = 0;
+            while (currentN >= M)
+            {
+      
+                if (N / 2 == (currentN - M))//5/2==3-2=1
+                {
+                    currentN = (currentN - M) / Y;
+                    countOfTargets++;
+                }
+                else
+                {
+                    currentN -= M;//3,1
+                    countOfTargets++;//2
+                }
+                
+            }
+            Console.WriteLine(currentN);
+            Console.WriteLine(countOfTargets);
+        }
+    }
+}
